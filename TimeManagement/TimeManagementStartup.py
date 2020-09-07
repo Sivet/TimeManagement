52ca33ce23c3070dc70474cd38c6023e7b31237b
Sivet <14292867+Sivet@users.noreply.github.com>
2020-09-07 09:17:34 +0200

    Fixed first time run error

--- TimeManagement/TimeManagementStartup.py
+++ TimeManagement/TimeManagementStartup.py
@@ -5,6 +5,12 @@ import TimeManagementResources as src
 
 now = datetime.now()
 
+def writeInitTime(writer):
+    row = [now.strftime("%V"), now.date(), src.weekDay(now.weekday()), now.time().strftime('%H:%M:%S'), '-', '-', '-']
+    print(row)
+    writer.writerow(row)
+
+
 if not os.path.isfile(src.path):
     print("Found no file")
     header = ['WeekNum', 'Date', 'DayOfWeek', 'StartTime', 'EndTime', 'TimeToday', 'TimeMonth']
@@ -12,7 +18,9 @@ if not os.path.isfile(src.path):
         with open(src.path, 'w+', newline='\n') as f:
             writer = csv.writer(f, delimiter=';')
             writer.writerow(header)
+            writeInitTime(writer)
             f.close()
+            exit()
     except IOError:
         print("Could not open file")
 
@@ -23,10 +31,9 @@ if lastLineDate.date() == now.date():
     exit()
 
 
-#Write initial info to file to save 'now'
-row = [now.strftime("%V"), now.date(), src.weekDay(now.weekday()), now.time().strftime('%H:%M:%S'), '-', '-', '-']
-print(row)
+# Write initial info to file to save 'now'
 with open(src.path, 'a+', newline='\n') as f:
-    writer = csv.writer(f, delimiter=';')
-    writer.writerow(row)
-    f.close()
+        writer = csv.writer(f, delimiter=';')
+        writeInitTime(writer)
+        f.close()
+
