cf25dd8ebb20478a2ffce381a72928c279f1a672
Simon <simon@rspsystems.com>
2021-06-30 15:19:17 +0200

    running as user

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if(($EUID != 0)); then
-	echo "Please run as sudo"
+if(($EUID == 0)); then
+	echo "Do not run as sudo"
 	exit
 fi
-u="$SUDO_USER"
+#u="$USER"
 cp -r TimeManagement/ /usr/local/bin/ &&
-chown -R $u:$u /usr/local/bin/TimeManagement &&
-cp worktimer.service /etc/systemd/user/ &&
+#chown -R $u:$u /usr/local/bin/TimeManagement &&
+sudo cp worktimer.service /etc/systemd/user/ &&
 systemctl --user daemon-reload &&
 systemctl start --user worktimer &&
 systemctl enable --user worktimer
